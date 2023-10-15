dee7f5ea468bf227e97ac96f519a44dfa2e82285
Danny Garcia <dgarcia1724@berkeley.edu>
2023-10-14 21:46:57 -0700

    form logic done!

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,30 +1,106 @@
 import { useReducer } from "react";
 import WhiteBtn from "../components/whiteBtn";
 
+const initialState = {
+  errorName: false,
+  errorEmailAddress: false,
+  errorPhone: false,
+  errorMessage: false,
+  emailMessage: "",
+  name: "",
+  emailAddress: "",
+  phone: "",
+  message: "",
+};
+
 function reducer(state, action) {
   switch (action.type) {
+    // name
+    case "nameTrue":
+      return { ...state, errorName: true };
+    case "nameFalse":
+      return { ...state, errorName: false };
+    // email
+    case "emailAddressTrue":
+      return { ...state, errorEmailAddress: true };
+    case "emailAddressFalse":
+      return { ...state, errorEmailAddress: false };
+    // phone
+    case "phoneTrue":
+      return { ...state, errorPhone: true };
+    case "phoneFalse":
+      return { ...state, errorPhone: false };
+    // message
+    case "messageTrue":
+      return { ...state, errorMessage: true };
+    case "messageFalse":
+      return { ...state, errorMessage: false };
+    // Email Message
+    case "emailEmpty":
+      return { ...state, emailMessage: "Email Address cannot be empty" };
+    case "emailWrongFormat":
+      return { ...state, emailMessage: "Please use a valid email address" };
+    // Other
     case "setName":
       return { ...state, name: action.payload };
-    case "setNameIsEmpty":
-      return { ...state, nameIsEmpty: action.payload };
+    case "setEmailAddress":
+      return { ...state, emailAddress: action.payload };
+    case "setPhone":
+      return { ...state, phone: action.payload };
+    case "setMessage":
+      return { ...state, message: action.payload };
     default:
       throw new Error("Unknown Action");
   }
 }
 
-const initialState = {
-  name: "",
-  nameIsEmpty: false,
-};
-
 function Contact() {
-  const [{ name, nameIsEmpty }, dispatch] = useReducer(reducer, initialState);
+  const [
+    {
+      errorName,
+      errorEmailAddress,
+      errorPhone,
+      errorMessage,
+      emailMessage,
+      name,
+      emailAddress,
+      phone,
+      message,
+    },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
   function handleSubmit() {
+    // name
     if (name === "") {
-      dispatch({ type: "setNameIsEmpty", payload: true });
+      dispatch({ type: "nameTrue" });
     } else {
-      dispatch({ type: "setNameIsEmpty", payload: false });
+      dispatch({ type: "nameFalse" });
+    }
+
+    // email
+    if (emailAddress === "") {
+      dispatch({ type: "emailAddressTrue" });
+      dispatch({ type: "emailEmpty" });
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailAddress)) {
+      dispatch({ type: "emailAddressTrue" });
+      dispatch({ type: "emailWrongFormat" });
+    } else {
+      dispatch({ type: "emailAddressFalse" });
+    }
+
+    // phone
+    if (phone === "") {
+      dispatch({ type: "phoneTrue" });
+    } else {
+      dispatch({ type: "phoneFalse" });
+    }
+
+    // message
+    if (message === "") {
+      dispatch({ type: "messageTrue" });
+    } else {
+      dispatch({ type: "messageFalse" });
     }
   }
 
@@ -46,6 +122,7 @@ function Contact() {
           </p>
         </section>
         <section className="text-center">
+          {/* Name Input */}
           <div className="mb-[24px] relative">
             <input
               className="formInput font-medium text-[15px] tracking-[0px] bg-transparent"
@@ -56,7 +133,7 @@ function Contact() {
                 dispatch({ type: "setName", payload: e.target.value })
               }
             />
-            {nameIsEmpty && (
+            {errorName && (
               <div className="errorMessage flex items-center gap-[9px]">
                 <p className="italic text-[12px] tracking-[0px]">
                   Can't be empty
@@ -68,9 +145,82 @@ function Contact() {
               </div>
             )}
           </div>
-          <WhiteBtn>
-            <div onClick={handleSubmit}>SUBMIT</div>
-          </WhiteBtn>
+          {/* Email Address Input */}
+          <div className="mb-[24px] relative">
+            <input
+              className="formInput font-medium text-[15px] tracking-[0px] bg-transparent"
+              type="text"
+              placeholder="Email Address"
+              value={emailAddress}
+              onChange={(e) =>
+                dispatch({ type: "setEmailAddress", payload: e.target.value })
+              }
+            />
+            {errorEmailAddress && (
+              <div className="errorMessage flex items-center gap-[9px]">
+                <p className="italic text-[12px] tracking-[0px]">
+                  {emailMessage}
+                </p>
+                <img
+                  src="src\designo-multi-page-website\starter-code\assets\contact\desktop\icon-error.svg"
+                  alt="icon-error"
+                />
+              </div>
+            )}
+          </div>
+          {/* Phone Input */}
+          <div className="mb-[24px] relative">
+            <input
+              className="formInput font-medium text-[15px] tracking-[0px] bg-transparent"
+              type="text"
+              placeholder="Phone"
+              value={phone}
+              onChange={(e) =>
+                dispatch({ type: "setPhone", payload: e.target.value })
+              }
+            />
+            {errorPhone && (
+              <div className="errorMessage flex items-center gap-[9px]">
+                <p className="italic text-[12px] tracking-[0px]">
+                  Can't be empty
+                </p>
+                <img
+                  src="src\designo-multi-page-website\starter-code\assets\contact\desktop\icon-error.svg"
+                  alt="icon-error"
+                />
+              </div>
+            )}
+          </div>
+          {/* Message Input */}
+          <div className="mb-[24px] relative">
+            <textarea
+              className="formInput font-medium text-[15px] tracking-[0px]
+              bg-transparent"
+              placeholder="Your Message"
+              value={message}
+              onChange={(e) =>
+                dispatch({ type: "setMessage", payload: e.target.value })
+              }
+            ></textarea>
+            {errorMessage && (
+              <div className="errorMessage flex items-center gap-[9px]">
+                <p className="italic text-[12px] tracking-[0px]">
+                  Can't be empty
+                </p>
+                <img
+                  src="src\designo-multi-page-website\starter-code\assets\contact\desktop\icon-error.svg"
+                  alt="icon-error"
+                />
+              </div>
+            )}
+          </div>
+          {/* Button */}
+          <button
+            className="py-[17px] px-[24px]  rounded-lg font-medium text-[15px] tracking-[1px] bg-white text-secondaryDarkGray cursor-pointer hover:bg-secondaryLightPeach hover:text-white"
+            onClick={handleSubmit}
+          >
+            SUBMIT
+          </button>
         </section>
       </div>
       {/* fake box delete later */}
