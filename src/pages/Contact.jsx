6cc679f1aaae0197ab064f72b1cb919815d9dc9c
Danny Garcia <dgarcia1724@berkeley.edu>
2023-10-14 22:44:05 -0700

    fin

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,7 @@
+import { Link } from "react-router-dom";
+import CircleBg from "../components/CircleBg";
+import PeachBtn from "../components/PeachBtn";
+
 import { useReducer } from "react";
 import WhiteBtn from "../components/whiteBtn";
 
@@ -102,26 +106,33 @@ function Contact() {
     } else {
       dispatch({ type: "messageFalse" });
     }
+
+    // Danny Garcia
+    alert("Looking forward to working with you :) dgarcia1724@berkeley.edu");
   }
 
   return (
     <div>
       <div
         className="bg-primaryPeach text-white py-[72px] px-[24px]
-      grid gap-[48px]"
+      grid gap-[48px] sm:py-[71px] sm:px-[58px] sm:gap-[40px] sm:mx-[40px] sm:rounded-[15px]
+      md:py-[54px] md:px-[96px] md:mx-[164px] md:grid-cols-2 md:gap-[0px] md:h-[480px]"
       >
-        <section className="text-center">
-          <h1 className="mb-[24px] font-medium text-[32px] tracking-[0px]">
+        <section className="text-center sm:text-left md:p-[48px]">
+          <h1 className="mb-[24px] font-medium text-[32px] tracking-[0px] sm:mb-[32px] sm:text-[48px]">
             Contact Us
           </h1>
-          <p className="font-normal text-[15px] leading-[25px] tracking-[0px]">
+          <p
+            className="font-normal text-[15px] leading-[25px] tracking-[0px]
+          sm:text-[16px] sm:leading-[26px]"
+          >
             Ready to take it to the next level? Let’s talk about your project or
             idea and find out how we can help your business grow. If you are
             looking for unique digital experiences that’s relatable to your
             users, drop us a line.
           </p>
         </section>
-        <section className="text-center">
+        <section className="text-center sm:text-right">
           {/* Name Input */}
           <div className="mb-[24px] relative">
             <input
@@ -223,8 +234,54 @@ function Contact() {
           </button>
         </section>
       </div>
-      {/* fake box delete later */}
-      <div className="w-full h-[400px] bg-cyan-300"></div>
+      {/* 3 Buttons Grid */}
+      <section className="my-[120px] pb-[286px] sm:pb-[384px] md:pb-[380px] mx-[12px] grid gap-[48px] sm:gap-[80px] md:my-[160px] md:mx-[164px] md:grid-cols-3 md:gap-[30px]">
+        {/* #1 */}
+        <div className="text-center">
+          <CircleBg>
+            <img
+              src="src\designo-multi-page-website\starter-code\assets\shared\desktop\illustration-canada.svg"
+              alt="illustration-canada"
+            />
+          </CircleBg>
+          <h1 className="mt-[48px] mb-[32px] font-medium text-[20px] tracking-[5px] text-secondaryDarkGray sm:mb-[48px]">
+            CANADA
+          </h1>
+          <Link to="/locations">
+            <PeachBtn>SEE LOCATION</PeachBtn>
+          </Link>
+        </div>
+        {/* #2 */}
+        <div className="text-center">
+          <CircleBg>
+            <img
+              src="src\designo-multi-page-website\starter-code\assets\shared\desktop\illustration-australia.svg"
+              alt="illustration-australia"
+            />
+          </CircleBg>
+          <h1 className="mt-[48px] mb-[32px] font-medium text-[20px] tracking-[5px] text-secondaryDarkGray sm:mb-[48px]">
+            AUSTRALIA
+          </h1>
+          <Link to="/locations">
+            <PeachBtn>SEE LOCATION</PeachBtn>
+          </Link>
+        </div>
+        {/* #3 */}
+        <div className="text-center">
+          <CircleBg>
+            <img
+              src="src\designo-multi-page-website\starter-code\assets\shared\desktop\illustration-united-kingdom.svg"
+              alt="illustration-united-kingdom"
+            />
+          </CircleBg>
+          <h1 className="mt-[48px] mb-[32px] font-medium text-[20px] tracking-[5px] text-secondaryDarkGray sm:mb-[48px]">
+            UNITED KINGDOM
+          </h1>
+          <Link to="/locations">
+            <PeachBtn>SEE LOCATION</PeachBtn>
+          </Link>
+        </div>
+      </section>
     </div>
   );
 }
